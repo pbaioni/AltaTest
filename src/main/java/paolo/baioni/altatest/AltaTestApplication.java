e9a98fec2d61f48458ea95fa35d378dd5ce3f864
Paolo Baioni <baioni2002@yahoo.fr>
2024-09-24 20:55:28 +0200

    first commit

---
+++ src/main/java/paolo/baioni/altatest/AltaTestApplication.java
@@ -0,0 +1,14 @@
+package paolo.baioni.altatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AltaTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AltaTestApplication.class, args);
+		System.out.println("start");
+	}
+
+}
